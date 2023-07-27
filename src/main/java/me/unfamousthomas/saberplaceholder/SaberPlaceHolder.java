9da21a3a6c661c848ec784f11219107c225d8512
UnfamousThomas <thompalts@gmail.com>
2023-07-27 16:31:39 +0300

    Add another placeholder + refractor

--- src/main/java/me/unfamousthomas/saberplaceholder/SaberPlaceHolder.java
+++ src/main/java/me/unfamousthomas/saberplaceholder/SaberPlaceHolder.java
@@ -1,17 +1,12 @@
 package me.unfamousthomas.saberplaceholder;
 
-import me.clip.placeholderapi.PlaceholderAPI;
 import me.unfamousthomas.saberplaceholder.placeholders.FactionPointsPlaceholder;
-import org.bukkit.ChatColor;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
  * @author thomp (11/06/2023)
  */
-public class SaberPlaceHolder extends JavaPlugin implements Listener {
+public class SaberPlaceHolder extends JavaPlugin{
 
     /**
      * What to do when plugin starts
@@ -20,15 +15,6 @@ public class SaberPlaceHolder extends JavaPlugin implements Listener {
     public void onEnable() {
         new FactionPointsPlaceholder().register();
 
-        //Bukkit.getPluginManager().registerEvents(this,this); //NB: This is used for testing. Unncoment it for testing again.
     }
 
-    @EventHandler
-    public void onJoinEvent(PlayerJoinEvent event) {
-        String join = "%custompointslist%";
-        join = PlaceholderAPI.setPlaceholders(event.getPlayer(), join);
-
-        event.setJoinMessage(ChatColor.translateAlternateColorCodes('&', join));
-
-    }
 }
--- src/main/java/me/unfamousthomas/saberplaceholder/placeholders/FactionPointsPlaceholder.java
+++ src/main/java/me/unfamousthomas/saberplaceholder/placeholders/FactionPointsPlaceholder.java
@@ -65,8 +65,74 @@ public class FactionPointsPlaceholder extends PlaceholderExpansion {
      */
     @Override
     public @Nullable String onRequest(OfflinePlayer player, @NotNull String params) {
-        return generateFactionString(player);
+        if(params.isBlank() || params.isEmpty()) return generateFactionString(player);
+        else if(params.equalsIgnoreCase("player")) return generateFactionRankingForPlayer(player);
+        return generateSpecificFactionString(params);
 
+    }
+
+    private String generateFactionRankingForPlayer(OfflinePlayer player) {
+        FPlayer fp = FPlayers.getInstance().getByOfflinePlayer(player);
+        if(!fp.hasFaction() || isInWildSafeWar(ChatColor.stripColor(fp.getFaction().getTag())))
+            return ChatColor.translateAlternateColorCodes('&', "&cYou are not part of any faction! Please join a faction to see its ranking");
+
+        Faction f = fp.getFaction();
+        int i = 1;
+        for (Faction fact : Factions.getInstance().getAllFactions()) {
+            if(fact.equals(f)) break;
+            if(!isInWildSafeWar(ChatColor.stripColor(fact.getTag()))) i += 1;
+        }
+
+        return ChatColor.translateAlternateColorCodes('&', "&a" + i + ".&f " + ChatColor.stripColor(f.getTag()) + ": &a" + f.getPoints());
+
+    }
+
+    /**
+     * Method to generate specific factions placeholder (when params)
+     * @param params Parameters after "_"
+     * @return String with colors
+     */
+    private String generateSpecificFactionString(String params) {
+        if(isFactionRank(params)) {
+            int num = Integer.parseInt(params);
+            ArrayList<Faction> factions = Factions.getInstance().getAllFactions();
+            //Sorts factions based on getPoints(). Very brute-force sorting, but should work in this scenario.
+            factions.sort(Comparator.comparingInt(Faction::getPoints));
+            Collections.reverse(factions); //Because col.sort sorts in ascending by default
+            //Finds faction with correct num. This would probably possible with some logic of indexOf, but since we do not know where warzone, wilderness and safezone are exactly, did this solution for now.
+            int i = 1;
+            Faction factionResult = null;
+            for (Faction faction : factions) {
+                if(i == num) {
+                    factionResult = faction;
+                }
+                if(!isInWildSafeWar(ChatColor.stripColor(faction.getTag()))) i +=1;
+            }
+            //If we did not find the faction sent it here.
+            if(factionResult == null) {
+                return ChatColor.translateAlternateColorCodes('&', "&cA faction with that rank does not exist");
+            } else {
+                //If we found one send the correct msg with rank + points
+                return ChatColor.translateAlternateColorCodes('&', "&a" + num + ".&f " + ChatColor.stripColor(factionResult.getTag()) + ": &a" + factionResult.getPoints());
+            }
+        } else {
+            return ChatColor.translateAlternateColorCodes('&', "&cA faction with that rank does not exist");
+        }
+    }
+
+    /**
+     * Basic checks: is params a num, and is there enough factions for such a faction rank to exist
+     * @param params Parameter we check against
+     * @return If such a ranking could exist
+     */
+    private boolean isFactionRank(String params) {
+        try {
+            int num = Integer.parseInt(params);
+            int totalFac = Factions.getInstance().getAllFactions().size()-3;
+            return totalFac >= num;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 
     /**
@@ -80,6 +146,7 @@ public class FactionPointsPlaceholder extends PlaceholderExpansion {
         ArrayList<Faction> factions = Factions.getInstance().getAllFactions();
         //Sorts factions based on getPoints(). Very brute-force sorting, but should work in this scenario.
         factions.sort(Comparator.comparingInt(Faction::getPoints));
+        Collections.reverse(factions); //Because col.sort sorts in ascending by default
         if(factions.isEmpty() || factions.size() < 4) return "&c&lNo factions found.";
         StringBuilder sb = new StringBuilder();
         int i = 1;
@@ -94,7 +161,7 @@ public class FactionPointsPlaceholder extends PlaceholderExpansion {
             if(isInWildSafeWar(tag)) continue;
             //If we have not added 10 add to ranking
             if(i <= 11) {
-                sb.append("&a").append(i).append(". ").append("&f").append(tag).append(": &a").append(faction.getPoints());
+                sb.append("&a").append(i).append(". ").append("&f").append(tag).append("&8: &a").append(faction.getPoints());
                 sb.append("\n");
             }
             //Add players faction in last line.
@@ -110,10 +177,10 @@ public class FactionPointsPlaceholder extends PlaceholderExpansion {
             sb.append("&f...");
             sb.append("\n");
             Faction faction = fp.getFaction();
-            sb.append("&a").append(personalFacPlacing).append(". ").append("&f").append(faction.getTag()).append(": &a").append(faction.getPoints());
+            sb.append("&a").append(personalFacPlacing).append(". ").append("&f").append(faction.getTag()).append("&8: &a").append(faction.getPoints());
 
         }
-        return sb.toString();
+        return ChatColor.translateAlternateColorCodes('&', sb.toString());
 
     }
 
@@ -126,8 +193,6 @@ public class FactionPointsPlaceholder extends PlaceholderExpansion {
         tag = ChatColor.stripColor(tag);
         if(tag.equalsIgnoreCase("wilderness")) return true;
         if(tag.equalsIgnoreCase("warzone")) return true;
-        if(tag.equalsIgnoreCase("safezone")) return true;
-
-        return false;
+        return tag.equalsIgnoreCase("safezone");
     }
 }
